a27decf65ec186c487c38662d36f9b92e3a1690b
Tony Jiang <tonyjinag917@gmail.com>
2019-10-22 10:29:07 -0400

    now redirects to index.html on successful form input

--- credentials/login.js
+++ credentials/login.js
@@ -14,18 +14,19 @@ function fieldBlur(field, text) {
 
 function testFunction() {
 	var form = document.getElementById("pwForm");
-	var uName = form.elements[0].value
-	var pWord = form.elements[1].value
+	var un = form.elements[0].value
+	var pw = form.elements[1].value
 	//Delete the following line after testing
-	console.log('username: ' + uName + ' password: ' + pWord)
+	console.log('username: ' + un + ' password: ' + pw)
 	//I need to get the usernames and passwords in an array somehow
 	var objPeople = [
 		{username: 'tonydagenius', password: '12345'}, 
 		{username: 'TsundereKermit', password: '12345'}
 	];
 	for(var i = 0; i < objPeople.length; i++) {
-		if(uName == objPeople[i].username && pWord == objPeople[i].password) {
-			console.log(username.value + " is logged in!!!")
+		if(un == objPeople[i].username && pw == objPeople[i].password) {
+			console.log(un + " is logged in!!!")
+			window.location.replace("../index.html");
 			return
 		}
 	}
