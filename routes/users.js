3e72187b41cf433e7f937f30239c7e2f098b1af8
Tony Jiang <tonyjiang917@gmail.com>
2019-10-26 11:58:41 -0400

    added some logic and pw encryption to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const User = require('../model/User')
 
 router.get('/login', (req, res) => res.render('login'));
 
 router.get('/register', (req, res) => res.render('register'));
 
-router.post('/register');
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match'});
+    }
+
+    if (password.length < 6) {
+        errors.push({ msg: 'Password should be at least 6 characters' });
+    }
+
+    if(errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        User.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                errors.push( {msg: 'Email is already registered'} );
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            } else {
+                const newUser = new User({
+                    name, 
+                    email,
+                    password
+                });
+
+                console.log(newUser);
+                res.send('Hello!')
+            }
+        })
+        .catch();
+    }
+});
+
 
 module.exports = router;
