2e294b8688c244d1e8937f5644b1c0b930615ed1
Tony Jiang <tjian1@ocdsb.ca>
2019-10-29 13:25:57 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-dbPassword = 'mongodb://TsundereKermit:2WgOLyH6gWEz5EwU@bubble-database-shard-00-00-mgypj.gcp.mongodb.net:27017,bubble-database-shard-00-01-mgypj.gcp.mongodb.net:27017,bubble-database-shard-00-02-mgypj.gcp.mongodb.net:27017/test?ssl=true&replicaSet=bubble-database-shard-0&authSource=admin&retryWrites=true&w=majority';
+//Replace with MongoDB connection string
+dbPassword = '';
 
 module.exports = {
     MongoURI: dbPassword
