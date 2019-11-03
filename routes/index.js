ffb3d93fc88bbfd3b4438d792a4ff002bf2e9fe8
Tony Jiang <tonyjiang917@gmail.com>
2019-11-02 22:51:59 -0400

    added content and logic to NewRoomForm.ejs

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const router = express.Router();
  * const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
  * Not completely sure how to implement this yet
  */
+const Chatkit = require('@pusher/chatkit-server');
+
+//New ChatKit instance
+const chatkit = new Chatkit.default({
+    instanceLocator: 'v1:us1:c8c2181d-9998-47f7-afab-c3978ecf675c',
+    key: 'd5eb01bb-3869-4621-a4a9-4ca902a83460:v3tYLijCNYuz65zdgorTxF1wybw2wVvbX924i0rByNM=',
+});
 
 //When localhost:5000 is called
 router.get('/', (req, res) => res.render('welcome', {title: 'Welcome to Bubble'}));
@@ -14,5 +21,35 @@ router.get('/index', (req, res) =>
         name: req.user.name,
         title: 'Bubble'
     }));
+
+router.post('/newRoom', (req, res) => {
+    const { inputUser, roomName, roomId } = req.body;
+    let errors = []
+
+    if (!roomName || !roomId) {
+        console.log(roomName + ' ' + roomId);
+        errors.push({ msg: 'Please fill in all fields' })
+    }
+
+    if (errors.length > 0) {
+        res.render('index', { name: req.user.name, errors, title: 'Bubble' })
+    } else {
+        console.log(inputUser + ' ' + roomName + ' ' + roomId);
+        chatkit.createRoom({
+            id: inputUser,
+            creatorId: inputUser,
+            name: roomName,
+        })
+        .then(() => {
+            console.log('Room created successfully');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+        req.flash('success_msg', 'Room created');
+        res.render('index', { name: req.user.name, title: 'Bubble' })
+    }
+});
 
 module.exports = router;
