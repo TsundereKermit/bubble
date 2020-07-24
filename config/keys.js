4d227a69309944bdbbe12e73948f0c8329d419ee
Tony Jiang <tonyjiang917@gmail.com>
2020-07-24 15:48:31 -0400

    Removed mongoDB key from public access

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://TsundereKermit:2WgOLyH6gWEz5EwU@bubble-database-shard-00-00-mgypj.gcp.mongodb.net:27017,bubble-database-shard-00-01-mgypj.gcp.mongodb.net:27017,bubble-database-shard-00-02-mgypj.gcp.mongodb.net:27017/test?ssl=true&replicaSet=bubble-database-shard-0&authSource=admin&retryWrites=true&w=majority';
+dbPassword = '';
 
 module.exports = {
     MongoURI: dbPassword
